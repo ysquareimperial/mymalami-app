8f6826022e6dd6d68b30daf2bccc5fd357467403
Zee530 <Zeee530@gmail.com>
2022-04-25 15:37:56 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavigation from './Routes/AppNavigation';
+import Navbar from './Dashboard.js/Navbar';
 function App() {
   return (
     <div>
+      <Navbar />
       <AppNavigation />
     </div>
   );
--- src/Dashboard.js/Navbar.js
+++ src/Dashboard.js/Navbar.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { ArrowDownCircle, Circle} from 'react-feather'
-import { Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Row } from 'reactstrap'
+import { Button, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Row } from 'reactstrap'
 import './Navbar.css'
 import blackhijab from '../images/blackhijab.jpg'
+import logo from '../figma-mymalami/logo.png'
+// import { Button } from 'bootstrap'
 export default function Navbar() {
     const [open, setOpen] = useState(false);
     const toggle = () => {
@@ -10,30 +12,51 @@ export default function Navbar() {
     };
     return (
         <div>
-            <Row>
-                <Col md={6}>
+            <Row className='navbar-main'>
+                <Col md={3}>
                     <p className='logo'>
-                        <Circle color='white' size='2.7em' style={{ backgroundColor: 'white', borderRadius: 20 }} />
+                        {/* <Circle color='white' size='2.7em' style={{ backgroundColor: 'white', borderRadius: 20 }} /> */}
+                        <img src={logo} alt='logo' style={{ width: 100, height: 45 }} />
                     </p>
                 </Col>
                 <Col md={6}>
-                    <Dropdown isOpen={open} toggle={toggle}>
+                    <Row className='topics'
+                    // style={{color: 'white', padding: 15, fontWeight: 'bolder'}}
+                    >
+                        <Col md={3}/>
+                        <Col>HOME</Col>
+                        <Col>FEATURES</Col>
+                        <Col>CONTACT</Col>
+                        <Col>ABOUT</Col>
+                        <Col md={3}/>
+                    </Row>
+                </Col>
+                <Col md={3}>
+                    <div style={{float: 'right', marginTop: 10, marginRight: 25}}>  
+                    <Row>
+                        <Col>
+                            <Button className='btn'>LOGIN</Button>
+                        </Col>
+                        <Col>
+                            <Button className='btn'>SIGNUP</Button>
+                        </Col>
+                    </Row>  
+                    </div>
+                    {/* <Dropdown isOpen={open} toggle={toggle}>
                         <DropdownToggle className='drop-down'>
                             <ArrowDownCircle />
                         </DropdownToggle>
                         <DropdownMenu className='drop-down-menu'>
-                            {/* <DropdownItem header>Settings & Privacy</DropdownItem> */}
-                            {/* <DropdownItem disabled>Action</DropdownItem> */}
                             <DropdownItem className='drop-down-item'>Settings & Privacy</DropdownItem>
                             <DropdownItem className='drop-down-item'>Help & Support</DropdownItem >
                             <DropdownItem className='drop-down-item' divider />
                             <DropdownItem className='drop-down-item'>Sign Out</DropdownItem>
                         </DropdownMenu>
-                    </Dropdown>
-                    <h1 className='dp-name'>
+                    </Dropdown> */}
+                    {/* <h1 className='dp-name'>
                         <img src={blackhijab} alt='profile' style={{ width: 38, height: 38, borderRadius: 50 }} />{" "}
                         Ms. Msmsms  
-                    </h1>
+                    </h1> */}
                 </Col>
             </Row>
         </div>
