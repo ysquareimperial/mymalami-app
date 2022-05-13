95278532dbdceb2ee6bfb787857e931767f4db78
Zee530 <Zeee530@gmail.com>
2022-05-13 14:40:53 +0100

    rrr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavigation from './Routes/AppNavigation';
-import Navbar from './Dashboard.js/Navbar';
+import Navbar from './Dashboard/Navbar.js';
 function App() {
   return (
     <div>
